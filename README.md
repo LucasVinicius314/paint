d516af59599436d453bda5f2d0eb056cff868ddb
Lucas Coelho <58010418+LucasVinicius314@users.noreply.github.com>
2024-03-07 10:10:06 -0300

    chore: moving TODOs

--- README.md
+++ README.md
@@ -1,10 +1,1 @@
 # paint
-
-TODO: add ctrl + z
-TODO: add panning
-TODO: vector layer to represent circle radius and center
-TODO: keybinds config/view
-TODO: raster selection tool (Q)
-TODO: hide vector lines config
-TODO: parametrize tooltip delay config
-TODO: vector node deletion tool
--- lib/main.dart
+++ lib/main.dart
@@ -4,3 +4,12 @@ import 'package:paint/core/app.dart';
 void main() {
   runApp(const App());
 }
+
+// TODO: add ctrl + z
+// TODO: add panning
+// TODO: vector layer to represent circle radius and center
+// TODO: keybinds config/view
+// TODO: raster selection tool (Q)
+// TODO: hide vector lines config
+// TODO: parametrize tooltip delay config
+// TODO: vector node deletion tool
